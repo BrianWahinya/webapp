ab3a851744a123a43ba88ec79ef69750cbf6e33d
bngure <brianwahinyangure@gmail.com>
2022-02-27 05:59:51 +0300

    Front_end App.test regex from getByText WebPortals to whitespace \s

--- front_end/portfolio/src/App.test.js
+++ front_end/portfolio/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Portfolio link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/WebPortals/i);
+  const linkElement = screen.getByText(/\s/i);
   expect(linkElement).toBeInTheDocument();
 });
