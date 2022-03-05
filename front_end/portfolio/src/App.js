9d859703fb6a0d8c7986aa444c642c138a8f6c97
bngure <brianwahinyangure@gmail.com>
2022-03-05 04:29:53 +0300

    Updated all packages. Removed error process not found by adding windows.process when componentDidMount in App.js. Configured App.js title to match current app

--- front_end/portfolio/src/App.js
+++ front_end/portfolio/src/App.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import './App.css';
-import Clock from './projects/clock_app/clock_main';
-import Weather from './projects/weather_app/weather_main';
-import QuoteGenerator from './projects/quotegenerator_app/quote_generator';
+import React from "react";
+import "./App.css";
+import Clock from "./projects/clock_app/clock_main";
+import Weather from "./projects/weather_app/weather_main";
+import QuoteGenerator from "./projects/quotegenerator_app/quote_generator";
 
 class App extends React.Component {
   selectApp = () => {
     const apps = {
-      'clock': <Clock/>,
-      'weather': <Weather/>,
-      'quotegenerator': <QuoteGenerator/>
-    }
+      clock: <Clock />,
+      weather: <Weather />,
+      quotegenerator: <QuoteGenerator />,
+    };
     const name = this.props.clickedProject;
     return apps[name];
+  };
+
+  componentDidMount() {
+    document.title = `Portal:${this.props.clickedProject}`;
+    window.process = {
+      ...window.process,
+    };
   }
-  render(){
+  componentDidUpdate() {
+    document.title = `Portal:${this.props.clickedProject}`;
+  }
+
+  render() {
     return <> {this.selectApp()} </>;
   }
 }
--- front_end/portfolio/src/projects/weather_app/weather_main.jsx
+++ front_end/portfolio/src/projects/weather_app/weather_main.jsx
@@ -1,50 +1,78 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "../../styles/weather.css";
 
 function Weather() {
-  const {REACT_APP_OPENWEATHER_API_KEY} = process.env;
+  const { REACT_APP_OPENWEATHER_API_KEY } = process.env;
+  console.log(process);
   const [weatherData, setWeatherData] = useState([]);
   const [city, setCity] = useState("");
 
   const getWeather = (event) => {
-    if(event.key === "Enter"){
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${REACT_APP_OPENWEATHER_API_KEY}`)
-        .then(data => data.json())
-        .then(jsondata => {
+    if (event.key === "Enter") {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${REACT_APP_OPENWEATHER_API_KEY}`,
+      )
+        .then((data) => data.json())
+        .then((jsondata) => {
           // console.log(jsondata);
           setWeatherData(jsondata);
           setCity("");
           event.target.value = "";
-        })
+        });
     }
-  }
+  };
 
   const formatWeather = (data) => {
     // console.log(data);
-    if(data.length !== 0){
-      if(data.name){
-        return <>
-          <h4>Country: {data.sys.country}</h4>
-          <h5>Place: {data.name}</h5>
-          <p>Wind =&gt; Speed:{data.wind.speed} Degree:{data.wind.deg} Gust:{data.wind.gust}</p>
-          <p><u>Condtions</u><br/>{data.weather.map(dw => <>Main:{dw.main}.<br/> Description:{dw.description}.<br/> Icon:{dw.icon}</>)}</p>
-        </>
+    if (data.length !== 0) {
+      if (data.name) {
+        return (
+          <>
+            <h4>Country: {data.sys.country}</h4>
+            <h5>Place: {data.name}</h5>
+            <p>
+              Wind =&gt; Speed:{data.wind.speed} Degree:{data.wind.deg} Gust:
+              {data.wind.gust}
+            </p>
+            <p>
+              <u>Condtions</u>
+              <br />
+              {data.weather.map((dw) => (
+                <>
+                  Main:{dw.main}.<br /> Description:{dw.description}.<br />{" "}
+                  Icon:{dw.icon}
+                </>
+              ))}
+            </p>
+          </>
+        );
       }
-      return <p>Error:{data.cod}.<br/>Message:{data.message}<br/>Advice: Kindly key in a valid city name</p>      
+      return (
+        <p>
+          Error:{data.cod}.<br />
+          Message:{data.message}
+          <br />
+          Advice: Kindly key in a valid city name
+        </p>
+      );
     }
-    return <>Please enter a city</>    
-  }
+    return <>Please enter a city</>;
+  };
 
-  return <div className="divWeather">
-    <input className="inputCity" placeholder="Enter City..." onKeyPress={getWeather} onChange={e => setCity(e.target.value)}/>
-    <div className="divCityWeather">{formatWeather(weatherData)}</div>
-  </div>
+  return (
+    <div className="divWeather">
+      <input
+        className="inputCity"
+        placeholder="Enter City..."
+        onKeyPress={getWeather}
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <div className="divCityWeather">{formatWeather(weatherData)}</div>
+    </div>
+  );
 }
 
 export default Weather;
-
-
-
 
 /*
 class Weather extends React.Component {
