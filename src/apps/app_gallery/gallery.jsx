6cdc78fa135ffe1ec41acfd60f8fc81b5dd03329
bngure <brianwahinyangure@gmail.com>
2022-06-13 20:08:32 +0300

    Gallery app added scss, used flexbox to display images, hover on image container displays photographer name. Start, previous, next and end pagination buttons. Select dropdown topics added

--- src/apps/app_gallery/gallery.jsx
+++ src/apps/app_gallery/gallery.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "./gallery.css";
 
 export default function Gallery() {
   const dataDefault = {
@@ -8,11 +9,26 @@ export default function Gallery() {
     photos: [],
   };
   const [data, setData] = useState(dataDefault);
+  const [page, setPage] = useState(1);
+  const [topic, setTopic] = useState("cars");
+
+  const topics = [
+    "ocean",
+    "coding",
+    "cars",
+    "soccer",
+    "dogs",
+    "cats",
+    "people",
+    "basketball",
+    "road",
+  ];
+
   useEffect(() => {
     const getData = async () => {
       try {
         const response = await fetch(
-          "https://api.pexels.com/v1/search?query=code&per_page=40",
+          `https://api.pexels.com/v1/search?query=${topic}&page=${page}&per_page=40&orientation=landscape`,
           {
             method: "GET",
             // mode: "no-cors",
@@ -33,21 +49,92 @@ export default function Gallery() {
 
     getData()
       .then((datos) => {
-        console.log(datos);
+        // console.log(datos);
         setData(datos ? datos : dataDefault);
       })
       .catch((err) => console.error(err.stack));
-  }, []);
+  }, [page, topic]);
+
+  const pageChange = (e) => {
+    const btn_target = e.target.id;
+    const maxPages = data.total_results / data.per_page;
+    switch (btn_target) {
+      case "start":
+        setPage(1);
+        break;
+      case "previous":
+        setPage(page - 1 === 0 ? 1 : page - 1);
+        break;
+      case "next":
+        setPage(page + 1 > maxPages ? 1 : page + 1);
+        break;
+      case "end":
+        setPage(maxPages);
+        break;
+      default:
+    }
+  };
+  const changeTopic = (e) => {
+    setTopic(e.target.value);
+    setPage(1);
+  };
+
   return (
     <>
       <h5>Gallery (coding in progress)</h5>
+      <p>From Pexels API</p>
       <p>Total: {data.total_results}</p>
       <p>
-        Page: {data.page} &nbsp; Per_page: {data.per_page}
+        <label htmlFor="topics">Topic:</label>
+        <select name="topics" onChange={changeTopic} value={topic}>
+          {topics.map((tp) => (
+            <option key={tp} value={tp}>
+              {tp}
+            </option>
+          ))}
+        </select>
+        &nbsp; Page: {data.page} &nbsp; Per_page: {data.per_page}
       </p>
-      {data.photos.map((photo) => (
-        <img key={photo.id} src={photo.src.small} alt={`${photo.alt}`} />
-      ))}
+      <div className="pagination">
+        <button className="btn btn-sm btn-info" onClick={pageChange} id="start">
+          Start
+        </button>
+        <button
+          className="btn btn-sm btn-secondary"
+          onClick={pageChange}
+          id="previous"
+        >
+          Previous
+        </button>
+        <button
+          className="btn btn-sm btn-success"
+          onClick={pageChange}
+          id="next"
+        >
+          Next
+        </button>
+        <button
+          className="btn btn-sm btn-warning"
+          onClick={pageChange}
+          id="end"
+        >
+          End
+        </button>
+      </div>
+      <div className="divGallery">
+        {data.photos.map((photo) => (
+          <div key={photo.id} className="containerImage">
+            <img
+              className="photo"
+              src={photo.src.small}
+              alt={`${photo.alt}`}
+              // onMouseEnter={imageHover}
+              // onMouseLeave={imageHoverOut}
+            />
+            <div className="photographer">Photo by: {photo.photographer}</div>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
