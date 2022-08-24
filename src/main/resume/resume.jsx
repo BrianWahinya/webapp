5101644039998c11859746487e11f341fd1215cb
bngure <brianwahinyangure@gmail.com>
2022-08-24 22:24:02 +0300

    Resume added nav-tabs education and work-experience

--- src/main/resume/resume.jsx
+++ src/main/resume/resume.jsx
@@ -1,5 +1,55 @@
-import React from "react";
+import "./resume.css";
 
 export default function Resume() {
-  return <div>Resume</div>;
+  return (
+    <div>
+      <h5>Resume</h5>
+      <nav>
+        <div className="nav nav-tabs" id="nav-tab" role="tablist">
+          <button
+            className="nav-link active"
+            id="nav-edu-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#nav-edu"
+            type="button"
+            role="tab"
+            aria-controls="nav-edu"
+            aria-selected="true"
+          >
+            Education
+          </button>
+          <button
+            className="nav-link"
+            id="nav-work-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#nav-work"
+            type="button"
+            role="tab"
+            aria-controls="nav-work"
+            aria-selected="false"
+          >
+            Work Experience
+          </button>
+        </div>
+      </nav>
+      <div className="tab-content" id="nav-tabContent">
+        <div
+          className="tab-pane fade show active"
+          id="nav-edu"
+          role="tabpanel"
+          aria-labelledby="nav-edu-tab"
+        >
+          This will display education details
+        </div>
+        <div
+          className="tab-pane fade"
+          id="nav-work"
+          role="tabpanel"
+          aria-labelledby="nav-work-tab"
+        >
+          This will display work-experience details
+        </div>
+      </div>
+    </div>
+  );
 }
