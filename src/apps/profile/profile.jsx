f5ba723955279e6e9b4239b280d3b1b2c1e31add
bngure <brianwahinyangure@gmail.com>
2022-08-26 04:50:05 +0300

    Package installed React-Typed and used typed-component in profile to display name and career

--- src/apps/profile/profile.jsx
+++ src/apps/profile/profile.jsx
@@ -1,11 +1,17 @@
+import Typed from "react-typed";
 import "./profile.css";
 
 export default function Profile() {
   return (
-    <div className="bgimg">
-      <div className="topleft">
-        <p>Brian</p>
-      </div>
+    <div className="profileDiv">
+      <Typed
+        strings={["I'm Brian Wahinya", "I'm a Software Engineer"]}
+        loop={true}
+        typeSpeed={100}
+        backSpeed={100}
+        backDelay={40}
+        smartBackspace={true}
+      />
     </div>
   );
 }
